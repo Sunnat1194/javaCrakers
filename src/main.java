aa7d7c48dac3746249242194eecbd54204859d78
day1 <Sunnat1194@gmail.com>
2020-11-18 16:47:34 -0500

    project

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hi group 20");
         System.out.println("How are you");
+        System.out.println("I hope everyone is doing great");
     }
 
 }
